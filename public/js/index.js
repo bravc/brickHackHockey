5175612ff071326c80e0a6b2a21976e5208449d1
Alex Taffe <alex.taffe@gmail.com>
2018-01-27 13:36:23 -0500

    Fix retina scaling

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,17 @@
+var pixelRatio = window.devicePixelRatio;
+
 $(document).ready(function() {
     var canvas = document.getElementById("gameBoard");
+    canvas.getContext('2d').scale(pixelRatio,pixelRatio);
 	var ctx = canvas.getContext("2d");
 	ctx.fillStyle = "#FF0000";
 	ctx.fillRect(0,0,150,75);
 });
 
+
+//make sure the canvas stays at 16:9
 $(window).resize(function(){
    $('#gameBoard').width($('#gameBoard').height() / (9/16));
 });
+
+
