396c73bb0cfba978c95ea3a282d37a43ab64771f
Alex Taffe <alex.taffe@gmail.com>
2018-01-27 13:28:45 -0500

    Fix style errors, add resize detection

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
-    var canvas = document.getElementById("myCanvas");
+    var canvas = document.getElementById("gameBoard");
 	var ctx = canvas.getContext("2d");
 	ctx.fillStyle = "#FF0000";
 	ctx.fillRect(0,0,150,75);
 });
 
+$(window).resize(function(){
+   $('#gameBoard').height($('#gameBoard').width() / (9/16));
+});
