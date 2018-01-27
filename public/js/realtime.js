d1c76312a1e1816211cc2e7ecfbda8c8788b5d14
Cameron <bravermancj@gmail.com>
2018-01-27 14:58:17 -0500

    added create room button

--- public/js/realtime.js
+++ public/js/realtime.js
@@ -4,7 +4,28 @@ const socket = io();
 
 
 $(document).ready(function() {
+
+    let roomID = $('#roomID'),
+        createBtn = $('#createRoomButton'),
+        joinBtn = $('#roomEnterButton');
+
+
+    createBtn.on('click', function(){
+        socket.emit("ADD_ROOM");
+    });
+
+    joinBtn.on('click', function(){        
+        if(roomID.val() != ''){
+            socket.emit("ROOM_CONNECT", )
+        }else{  
+            alert("User does not exist!")
+        }
+    });
+
     socket.on('connect', function(){
         console.log('Connected...'); 
     });
+
+
+    
 });
